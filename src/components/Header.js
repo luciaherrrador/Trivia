ae037ea0c1cd0c0e13405410559c9df34b61a65f
luciaherrrador <luciaherradorcarrasco@gmail.com>
2020-03-20 13:28:38 +0100

    add icons

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,11 +4,11 @@ import React from 'react';
 const Header = () => {
     return <header>
     <div className="container_links">
-    <a className="link link-nav" href="..."><span>BROWSE</span></a>
-    <a className="link link-nav" href="...">✚ ADD NEW QUESTION</a>
-    <a className="link link-nav" href="..."><span>API</span></a>
-    <a className="link link-nav" href="..."><span>DISCUSS</span></a>
-    <a className="link link-nav" href="..."><span>LOGIN</span></a>
+    <a className="link link-nav1" href="..."><span>BROWSE</span></a>
+    <a className="link link-nav2" href="...">✚ ADD NEW QUESTION</a>
+    <a className="link link-nav3" href="..."><span>API</span></a>
+    <a className="link link-nav4" href="..."><span>DISCUSS</span></a>
+    <a className="link link-nav5" href="..."><span>LOGIN</span></a>
 </div>
 </header>
 }
