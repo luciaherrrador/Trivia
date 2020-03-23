28a8135ee9ddaf80708f8291a64aa46cede60b00
luciaherrrador <luciaherradorcarrasco@gmail.com>
2020-03-23 09:35:37 +0100

    add docs folder

---
+++ docs/precache-manifest.8dbfe1b561c8885b3ee66445c5e69a4a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9fe9c6b3edac0cdf2ae336fbe1528d7",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b02107aa4f79d7089e53",
+    "url": "./static/css/main.93eb4cee.chunk.css"
+  },
+  {
+    "revision": "4893089b557cdfe22458",
+    "url": "./static/js/2.c2ad01a8.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "./static/js/2.c2ad01a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b02107aa4f79d7089e53",
+    "url": "./static/js/main.9884e4de.chunk.js"
+  },
+  {
+    "revision": "cddb77104b33339f6394",
+    "url": "./static/js/runtime-main.593a9f2a.js"
+  },
+  {
+    "revision": "6e2b7908985be1c768b5c2fc0ec18b89",
+    "url": "./static/media/gear.6e2b7908.svg"
+  },
+  {
+    "revision": "ba14b4454e6613d77a468d3c7207ce47",
+    "url": "./static/media/login.ba14b445.svg"
+  },
+  {
+    "revision": "5039d5e175dd68e56f3bc4ea9a27cfab",
+    "url": "./static/media/menu.5039d5e1.svg"
+  }
+]);
